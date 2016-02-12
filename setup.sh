84b24b003eec7fcc29e27264f68a29825ad48182
marcinthecloud <marc@marcinthecloud.com>
2016-02-12 10:29:18 -0800

    made script more efficient

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 cd
+wget https://github.com/Marcinthecloud/AzureTechDaySetup/archive/master.zip
+sudo apt-get install unzip
+unzip master.zip
 echo "installing needed packages"
 sudo apt-get install python-pip
 sudo apt-get install python-dev
 sudo pip install cassandra-driver
+cd AzureTechDaySetup-master/DSESetup/
 echo "Running data loader script..."
-python AzureTechDaySetup-master/DSE Setup/solr_dataloader.py
+python solr_dataloader.py
 echo "Creating and indexing Solr cores..."
-AzureTechDaySetup-master/DSE Setup/create_core.sh
+./create_core.sh
